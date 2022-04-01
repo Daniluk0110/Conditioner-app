f045162d7063abf243a89027b91d4fe04d9c9952
Daniel S <sech-daniil@yandex.ru>
2022-04-01 20:14:07 +0300

    add companies table, model, controller

---
+++ database/migrations/2022_04_01_170029_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+           $table->foreignId('company_id')->after('usd_price')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+        Schema::dropColumns('products', ['company_id']);
+    }
+};
