22d1fb287109fe2e5254e98d701fcddaed4a8b0a
Daniel S <sech-daniil@yandex.ru>
2022-04-01 20:25:00 +0300

    add product_property table

---
+++ database/migrations/2022_04_01_171948_create_product_property_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_property', function (Blueprint $table) {
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->foreignId('property_id')->references('id')->on('properties');
+            $table->string('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_property');
+    }
+};
