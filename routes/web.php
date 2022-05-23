420b471465c9fca2772f01438d84141e8fc5df18
Daniel S <sech-daniil@yandex.ru>
2022-05-23 23:14:37 +0300

    add table

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,8 @@ Route::get('/prices', function () {
 Route::get('/about', function () {
     return view('website.about');
 });
-Route::get('/products', function () {
-    return view('website.about');
-});
-Route::get('/products/{product}', []);
-
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
 
 Route::group(['prefix' => '/admin', 'middleware' => 'auth'], function () {
 
